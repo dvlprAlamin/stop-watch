9805eb2be7e2d4eb31f6eaf2072db99fe3f35a25
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-02-17 13:21:35 +0600

    clean up

--- app.js
+++ app.js
@@ -6,62 +6,34 @@ const startBtn = document.getElementById('start-btn');
 const stopBtn = document.getElementById('stop-btn');
 const resetBtn = document.getElementById('reset-btn');
 
-
+// two digit updater
 const isTwoDigit = unit => unit.innerText.length === 1 && (unit.innerText = '0' + unit.innerText);
-const resetUnit = (unit,limit) => unit.innerText > limit && (unit.innerText = '00') && unit.previousElementSibling.innerText++;
 
+// reset unit after reached max limit
+const resetUnit = (unit, limit) => unit.innerText > limit && (unit.innerText = '00') && unit.previousElementSibling.innerText++;
 
+// start button event handler
 startBtn.addEventListener('click', () => {
-    const interval = setInterval(() => {
+    const setTime = () => {
         centiSeconds.innerText++;
-        resetUnit(centiSeconds,99)
-        resetUnit(seconds,59)
-        resetUnit(minutes,59)
+        resetUnit(centiSeconds, 99)
+        resetUnit(seconds, 59)
+        resetUnit(minutes, 59)
         isTwoDigit(centiSeconds);
         isTwoDigit(seconds);
         isTwoDigit(minutes);
         isTwoDigit(hours);
-    }, 10);
-    
+    }
+    const interval = setInterval(setTime, 10);
+    // stop button event handler
     stopBtn.addEventListener('click', () => clearInterval(interval));
 })
+
+// reset button event handler
 const resetAll = () => {
     centiSeconds.innerText = seconds.innerText = minutes.innerText = hours.innerText = '00';
     stopBtn.className === '' && stopBtn.click();
 }
 
-
-
-
-
-
-
-
-
-// startBtn.addEventListener('click', () => {
-//     setTime(centiSeconds,10);
-//     setTime(seconds, 1000);
-//     setTime(minutes, 60000);
-//     setTime(hours, 3600000);
-// });
-
-// const setTime = (unit, count) => {
-//     let limit;
-//     const interval = setInterval(() => {
-//         unit.innerText++;
-//         unit.innerText.length === 1 && (unit.innerText = '0' + unit.innerText);
-//         unit.innerText === limit && (unit.innerText = '00')
-//     }, count);
-//     unit === seconds || minutes ? limit = '60' : unit === centiSeconds ? limit = '100' : limit = '24';
-//     stopBtn.addEventListener('click', () => clearInterval(interval));
-//     resetBtn.addEventListener('click', () => {
-//         centiSeconds.innerText = '00';
-//         seconds.innerText = '00';
-//         minutes.innerText = '00';
-//         hours.innerText = '00';
-//         stopBtn.className === '' && stopBtn.click();
-//     });
-// };
-
+// Start and Stop button toggler
 const toggleBtn = () => startBtn.classList.toggle('d-none') !== stopBtn.classList.toggle('d-none');
-
